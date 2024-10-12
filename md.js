f94b8b098c62267fa46624e2bd89e610ce609887
DoniLite <yaomessan13@outlook.com>
2024-10-12 18:42:24 +0000

    upload javascript

--- md.js
+++ md.js
@@ -1,28 +1,29 @@
-/* eslint-disable no-undef */
-document.querySelectorAll('pre').forEach(el => {
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('pre').forEach((el) => {
     const btn = document.createElement('button');
     btn.classList.add('copy-btn');
     btn.innerText = 'copier';
-    el.insertAdjacentHTML('beforeend', btn); 
-});
+    el.insertAdjacentHTML('beforeend', btn);
+  });
 
-document.querySelectorAll('.copy-btn').forEach((button) => {
-  button.addEventListener('click', function () {
-    const codeBlock = this.previousElementSibling;
-    const textToCopy = codeBlock.innerText;
+  document.querySelectorAll('.copy-btn').forEach((button) => {
+    button.addEventListener('click', function () {
+      const codeBlock = this.previousElementSibling;
+      const textToCopy = codeBlock.innerText;
 
-    // Copier dans le presse-papiers
-    navigator.clipboard
-      .writeText(textToCopy)
-      .then(() => {
-        // Changer temporairement le texte du bouton pour montrer que le code est copié
-        button.textContent = 'Copié !';
-        setTimeout(() => {
-          button.textContent = 'Copier';
-        }, 2000);
-      })
-      .catch((err) => {
-        console.error('Échec de la copie', err);
-      });
+      // Copier dans le presse-papiers
+      navigator.clipboard
+        .writeText(textToCopy)
+        .then(() => {
+          // Changer temporairement le texte du bouton pour montrer que le code est copié
+          button.textContent = 'Copié !';
+          setTimeout(() => {
+            button.textContent = 'copier';
+          }, 2000);
+        })
+        .catch((err) => {
+          console.error('Échec de la copie', err);
+        });
+    });
   });
 });
